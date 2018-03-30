07c2526e3cd7e427bff180aa13a44a3a308af6a5
ore-oluwa.olasubulumi <ore-oluwa.olasubulumi@open-16.CS.McGill.CA>
2018-03-30 19:41:51 -0400

    Just Started

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Mar 30, 2018
+ *      Author: ore-oluwa.olasubulumi
+ */
+//Ogo-Oluwa Jesutomi Olasubulumi <tomilashy@yahoo.com>
+
+
+
+#include <json.hpp>
+
+using json = nlohmann::json;
+
+
